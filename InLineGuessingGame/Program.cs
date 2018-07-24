8d01a253c4fac94c20c3c812c810f755d26aac77
Temeka <misstemeka1@live.com>
2018-07-23 20:47:55 -0500

    In-Line

--- InLineGuessingGame/Program.cs
+++ InLineGuessingGame/Program.cs
@@ -6,7 +6,18 @@ namespace InLineGuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Guess the correct # to win, using an in line statement
+
+            Console.Write("Guess and type a number between 1 and 10: ");
+            string userValue = Console.ReadLine();
+
+            //Using in line statement/operator, prompts the output based on 
+            //if the option chosen is "true" or "false"
+
+            string message = (userValue == "8") ? "You Win!" : "Sorry, you Lose.";
+
+            Console.WriteLine(message);
+            Console.ReadLine();
         }
     }
 }
